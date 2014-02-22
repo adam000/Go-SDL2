1a1d13442957ece8e98ca2ced434616a7808fd9f
Adam Hintz <adamh.zero@gmail.com>
2014-02-22 13:17:08 -0800

    GL: add comments

--- sdl/gl.go
+++ sdl/gl.go
@@ -7,6 +7,7 @@ type GLContext struct {
 	c C.SDL_GLContext
 }
 
+// Create a new GLContext in the given window
 func NewGLContext(w Window) (GLContext, error) {
 	context := C.SDL_GL_CreateContext(w.w)
 	if context == nil {
@@ -15,10 +16,12 @@ func NewGLContext(w Window) (GLContext, error) {
 	return GLContext{context}, nil
 }
 
+// Destroy the GLContext
 func (c GLContext) Destroy() {
 	C.SDL_GL_DeleteContext(c.c)
 }
 
+// "Swap the buffers" for GL applications
 func (w Window) GLSwap() {
 	C.SDL_GL_SwapWindow(w.w)
 }
