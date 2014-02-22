4747e4f5b0dd6e935d704ad35f7ef67ecc0839c4
Adam Hintz <adamh.zero@gmail.com>
2014-02-22 00:06:57 -0800

    Add SDL_GL functions for having a GL Context

---
+++ sdl/gl.go
@@ -0,0 +1,26 @@
+package sdl
+
+// #cgo pkg-config: sdl2
+//
+// #include "SDL.h"
+import "C"
+
+type GLContext struct {
+	c *C.SDL_GLContext
+}
+
+func NewGLContext(w Window) (GLContext, error) {
+	context := C.SDL_GL_CreateContext(w.w)
+	if context == nil {
+		return GLContext{}, GetError()
+	}
+	return GLContext{&context}, nil
+}
+
+func (c GLContext) Destroy() {
+	C.SDL_GL_DeleteContext(*c.c)
+}
+
+func (w Window) GLSwap() {
+	C.SDL_GL_SwapWindow(w.w)
+}
