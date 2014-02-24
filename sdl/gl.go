3f45f5098755a42f78a8bda0f02fdda6030803c9
Adam Hintz <adamh.zero@gmail.com>
2014-02-23 21:22:55 -0800

    Fix comments

--- sdl/gl.go
+++ sdl/gl.go
@@ -7,7 +7,7 @@ type GLContext struct {
 	c C.SDL_GLContext
 }
 
-// Create a new GLContext in the given window
+// NewGLContext creates a new GLContext in the given window
 func NewGLContext(w Window) (GLContext, error) {
 	context := C.SDL_GL_CreateContext(w.w)
 	if context == nil {
@@ -16,12 +16,13 @@ func NewGLContext(w Window) (GLContext, error) {
 	return GLContext{context}, nil
 }
 
-// Destroy the GLContext
+// Destroy deletes the GLContext
 func (c GLContext) Destroy() {
 	C.SDL_GL_DeleteContext(c.c)
 }
 
-// "Swap the buffers" for GL applications
+// GLSwap updates a window with an OpenGL rendering. (Used in double-buffering environments, which
+// are the default)
 func (w Window) GLSwap() {
 	C.SDL_GL_SwapWindow(w.w)
 }
